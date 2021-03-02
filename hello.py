df2dd6e285e2d6f90aaa3e1aeef290aa46450362
Saulo Coelho <coelho.saulo@gmail.com>
2021-03-02 14:28:56 -0300

    organizando em pasta

--- hello.py
+++
@@ -1,4 +0,0 @@
-line1='Olá, desenvolvedor Pyhton...'
-line2='Bem-vindo ao mundo do Python!!!'
-print(line1)
-print(line2)
--- input.py
+++
@@ -1,5 +0,0 @@
-first = input('Digite seu primeiro nome: ')
-last = input('Digite seu sobrenome: ')
-line1 = 'Olá ' + first + ' ' + last + '...'
-print(line1)
-print('Bem vindo ao mundo do Python!!!')
